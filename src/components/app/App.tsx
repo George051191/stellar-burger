24ab00ef92ec480b6dac0ed99af208b01781a831
George051191 <trubacheff_91@mail.ru>
2021-12-02 23:35:57 +0300

    feat: Настроить файловую структуру

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CheckMarkIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+function App() {
+  return (
+    
+    
+     <Logo/>
+   
+  );
+}
+
+export default App;
