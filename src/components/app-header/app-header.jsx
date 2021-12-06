1b60caa1627a4d70f81c540816331029916cebc8
George051191 <trubacheff_91@mail.ru>
2021-12-06 23:16:19 +0300

    feat:сделать верстку главной страницы

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -7,19 +7,19 @@ import { MenuItem } from "../menu-item/menu-item";
 export function AppHeader() {
   return (
     <header className={headerStyles.header}>
-      <nav className={headerStyles.navigation}>
-        <ul className={headerStyles.list}>
-          <MenuItem text='Конструктор'>
-            <BurgerIcon />
+      <nav className={headerStyles.header__navigation}>
+        <ul className={headerStyles.header__list}>
+          <MenuItem text='Конструктор' textStyles={'text text_type_main-default ml-2'} styles={`${headerStyles.header__item} pl-5 pr-5`}>
+            <BurgerIcon type='primary' />
           </MenuItem>
-          <MenuItem text='Лента заказов'>
-            <ListIcon />
+          <MenuItem text='Лента заказов' textStyles={'text text_type_main-default text_color_inactive ml-2'} styles={`${headerStyles.header__item} pl-5 pr-5`}>
+            <ListIcon type='secondary' />
           </MenuItem>
-          <li className={headerStyles.navigation__logo}>
+          <MenuItem styles={headerStyles.header__logo}>
             <Logo />
-          </li>
-          <MenuItem text='Личный кабинет'>
-            <ProfileIcon />
+          </MenuItem>
+          <MenuItem text='Личный кабинет' textStyles={'text text_type_main-default text_color_inactive ml-2'} styles={`${headerStyles.header__item} pl-5 pr-5`}>
+            <ProfileIcon type='secondary' />
           </MenuItem>
         </ul>
       </nav>
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import menuItemStyles from './menu-item.module.css'
+
 
 export function MenuItem(props) {
     return (
-        <li className={`pr-5 pl-5 mt-4 mb-4 mr-2 ${menuItemStyles.menu__item}`}>
+        <li className={props.styles}>
             {props.children}
-            <p className='text text_type_main-default mr-2 ml-2'>{props.text}</p>
+            {props.text && <p className={props.textStyles}>{props.text}</p>}
         </li>
     )
 }
