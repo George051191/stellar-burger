a28d1f09412d3a6f44dbc6fbef21200df1ff769b
George051191 <trubacheff_91@mail.ru>
2022-01-02 10:51:27 +0300

    feat: сделать класс апи

---
+++ src/components/utils/Api.js
@@ -0,0 +1,36 @@
+const baseUrl = 'https://norma.nomoreparties.space/api/';
+
+
+class Api {
+    constructor({ url }) {
+        this._url = url;
+        this._headers = {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    _checkResponse(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        // если ошибка, отклоняем промис
+        return Promise.reject(`Ошибка: ${res.status}`);
+    }
+
+
+    getBurgerIngredientsData() {
+        return fetch(`${this._url}ingredients`, {
+            method: 'GET',
+            headers: this._headers
+        }).then(this._checkResponse)
+    }
+
+
+
+
+
+}
+
+export default new Api({
+    url: baseUrl
+})
