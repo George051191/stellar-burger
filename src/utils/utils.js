27b95a5e7c10f16e06d2566811a5b9ed7bcdd915
George051191 <trubacheff_91@mail.ru>
2022-01-13 23:13:13 +0300

    fix: исправить замечания ревью

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 export const uid = () => { return Date.now() * Math.random() }
 
 export function calculateCost(elements, bunPrice = 0) {
-    return bunPrice + elements.reduce((res, item) => {
+    return (2 * bunPrice) + elements.reduce((res, item) => {
         return res + item.price
     }, 0)
 }
