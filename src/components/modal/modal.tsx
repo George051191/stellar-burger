f1e1b9334b0e244b51ec26eca03a3b33d9a0f411
George051191 <trubacheff_91@mail.ru>
2022-03-23 23:40:16 +0300

    add animation

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -4,10 +4,11 @@ import popupStyles from './modal.module.css';
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { ModalOverlay } from '../modal-overlay/modal-overlay';
 import { useEffect } from "react";
+import { CSSTransition } from 'react-transition-group'
 
 import { IModal } from "../../utils/interfaces";
 
-export const Modal:FunctionComponent<IModal> = (props) => {
+export const Modal: FunctionComponent<IModal> = (props) => {
 
   const modalRef = React.useRef<HTMLDivElement>(null);
   const modalRoot = document.getElementById('modal-root');
@@ -32,16 +33,22 @@ export const Modal:FunctionComponent<IModal> = (props) => {
   }, [])
 
   return ReactDOM.createPortal(
-    (<div className={popupStyles.popup} >
-      <ModalOverlay ref={modalRef} closeFunction={(evt: React.MouseEvent<HTMLDivElement>) => { closeByClickOverlay(evt) }} />
-      <div className={popupStyles.modal} >
-        <div className={props.modalHeaderStyles} >
-          {props.headerText && <h2 className={props.modalStyles} >{props.headerText}</h2>}
-          <div className={popupStyles.popup__closeicon}><CloseIcon type='primary' onClick={props.closeModal} /></div>
+
+      (<div className={popupStyles.popup} >
+        <ModalOverlay ref={modalRef} closeFunction={(evt: React.MouseEvent<HTMLDivElement>) => { closeByClickOverlay(evt) }} />
+
+        <div className={popupStyles.modal}  >
+
+          <div className={props.modalHeaderStyles} >
+            {props.headerText && <h2 className={props.modalStyles} >{props.headerText}</h2>}
+            <div className={popupStyles.popup__closeicon}><CloseIcon type='primary' onClick={props.closeModal} /></div>
+          </div>
+          {props.children}
         </div>
-        {props.children}
+
       </div>
-    </div>
-    ), modalRoot!)
+
+  ), modalRoot!)
 }
 
+//
